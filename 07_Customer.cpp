a23be1779c437c6c8294046f5da6b25e292cd49c
Akhil Saklani <akhilsaklani4@gmail.com>
2026-09-14 22:24:52 +0530

    feat: add Customer entity

---
+++ 07_Customer.cpp
@@ -0,0 +1,9 @@
+class Customer {
+private:
+    string name;
+    string phone;
+public:
+    Customer(const string& name, const string& phone) : name(name), phone(phone) {}
+    string getName() const { return name; }
+    string getPhone() const { return phone; }
+};
