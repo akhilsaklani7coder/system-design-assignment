4c14dd6cbe34df80e06f1a897084d450d0944ade
Akhil Saklani <akhilsaklani4@gmail.com>
2026-09-14 22:28:50 +0530

    refactor: explicitly demonstrate this keyword

--- 01_Movie.cpp
+++ 01_Movie.cpp
@@ -4,9 +4,12 @@ private:
     string language;
     int durationMinutes;
 public:
-    Movie() : title(""), language(""), durationMinutes(0) {} // compile-time polymorphism: overloaded constructor
-    Movie(const string& title, const string& language, int durationMinutes)
-        : title(title), language(language), durationMinutes(durationMinutes) {}
+    Movie() : title(""), language(""), durationMinutes(0) {} // Compile-time polymorphism: overloaded constructor.
+    Movie(const string& title, const string& language, int durationMinutes) {
+        this->title = title; // this keyword: refers to the current Movie object.
+        this->language = language;
+        this->durationMinutes = durationMinutes;
+    }
     string getTitle() const { return title; }
     string getLanguage() const { return language; }
     int getDurationMinutes() const { return durationMinutes; }
